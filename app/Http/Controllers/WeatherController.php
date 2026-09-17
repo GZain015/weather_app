67ccbb91783d8747d5a289d126fb42dc14154b06
Ghulam Zain <85392231+GZain015@users.noreply.github.com>
2026-09-17 10:15:11 +0500

    Update WeatherController.php

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -2,20 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class WeatherController extends Controller
 {
-    public function index(): string
-    {
-        return "This is a Weather App!";
-    }
+    // public function index(): string
+    // {
+    //     return "This is a Weather App!";
+    // }
 
     // public function show(string $city) : string 
     // {
     //     return "Showing Weather for {$city}";
     // }
+
+    public function index(): View
+    {
+        return View('weather.index');
+    }
+
+    public function search(Request $request) : RedirectResponse
+    {
+        $validated = $request->validate([
+            "city" => ['required', 'string', 'min:2', 'max:60'],
+        ]);
+
+        return redirect()->route('weather.show', ['city' => $validated['city']]);
+    }
 
     public function show(string $city) : View 
     { 
@@ -25,4 +41,5 @@ class WeatherController extends Controller
             'condition' => 'Partialy Clouded',
         ]);
     }
+
 }
