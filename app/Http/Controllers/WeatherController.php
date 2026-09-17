78cde2268d57f278634b104532ac69ce8935bbc0
Ghulam Zain <85392231+GZain015@users.noreply.github.com>
2026-09-17 11:04:42 +0500

    Update WeatherController.php

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Http;
 
 class WeatherController extends Controller
 {
@@ -25,11 +25,43 @@ class WeatherController extends Controller
 
     public function show(string $city) : View 
     { 
+        $location = Http::get('https://geocoding-api.open-meteo.com/v1/search', [
+            'name' => $city,
+            'count' => 1,
+        ])->json('results.0');
+
+        if($location == null){
+            abort(404, "We couldn't find a city called \"{$city}\".");
+        }
+
+        $current = Http::get('https://api.open-meto.com/v1/forecast', [
+            'latitude' => $location['latitude'],
+            'longitude' => $location['longitude'],
+            'current' => 'temperature_2m,weather_code,wind_speed_10m',
+            'timezone' => 'auto',
+        ])
+
         return view('weather.show', [
-            'city' => $city,
-            'temperature' => 30,
-            'condition' => 'Partialy Clouded',
+            'city' => $location['name'],
+            'country' => $location['country'],
+            'temperature' => $current['temperature_2m'],
+            'windSpeed' => $current['wind_speed_10m'],
+            'condition' => $this->describeWeatherCode($current['weather_code']),
         ]);
+    }
+
+    private function describeWeatherCode(int $code): string
+    {
+        return match (true) {
+            $code === 0 => 'Clear sky',
+            in_array($code, [1, 2, 3]) => 'Partly cloudy',
+            in_array($code, [45, 48]) => 'Foggy',
+            in_array($code, [51, 53, 55, 56, 57]) => 'Drizzle',
+            in_array($code, [61, 63, 65, 66, 67, 80, 81, 82]) => 'Rain',
+            in_array($code, [71, 73, 75, 77, 85, 86]) => 'Snow',
+            in_array($code, [95, 96, 99]) => 'Thunderstorm',
+            default => 'Unknown',
+        };
     }
 
 }
